f2b4e3e26e4008d8d84abe4f9ce3f42ad28f5a34
luislenin <luisleninlio@gmail.com>
2023-02-21 15:41:52 -0500

    pagina Gastos

---
+++ lib/pages/gastos.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:lecalianshop/pages/gastos.dart';
+
+class Gasto extends StatefulWidget {
+  const Gasto({super.key});
+
+  @override
+  State<Gasto> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Gasto> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
