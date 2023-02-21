8d8e8ef1514cf1a617b818ad2be127b4a97ea5e7
luislenin <luisleninlio@gmail.com>
2023-02-21 15:39:33 -0500

    cambios en home

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:lecalianshop/pages/gastos.dart';
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
 
@@ -8,8 +9,208 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  
+  late PageController _controller;
+  int currentPage = 9;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = PageController(
+      initialPage: currentPage,
+      viewportFraction: 0.4,
+    );
+  }
+
+  Widget _bottomAction(IconData icon) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(icon),
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8.0,
+        shape: const CircularNotchedRectangle(),
+        color: const Color.fromRGBO(13, 11, 38, 1),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _bottomAction(Icons.perm_identity,),
+            _bottomAction(Icons.public),
+            const SizedBox(width: 48.0),
+            _bottomAction(Icons.settings),
+            _bottomAction(Icons.logout),
+          ],
+        ),
+        
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const Gasto()));
+        },
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          _selector(),
+          _expenses(),
+          _graph(),
+          Container(
+            color: Colors.white,
+            height: 24.0,
+          ),
+          _list(),
+        ],
+      ),
+    );
+  }
+
+  Widget _pageItem(String name, int position) {
+    var _alignment;
+    const selected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Color.fromARGB(255, 5, 4, 0),
+    );
+    final unselected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.blueGrey.withOpacity(0.4),
+    );
+
+    if (position == currentPage) {
+      _alignment = Alignment.center;
+    } else if (position > currentPage) {
+      _alignment = Alignment.centerRight;
+    } else {
+      _alignment = Alignment.centerLeft;
+    }
+
+    return Align(
+      alignment: _alignment,
+      child: Text(name,
+        style: position == currentPage ? selected : unselected,
+      ),
+    );
+  }
+
+  Widget _selector() {
+    return SizedBox.fromSize(
+      size: const Size.fromHeight(70.0),
+      child: PageView(
+        onPageChanged: (newPage) {
+          setState(() {
+            currentPage = newPage;
+          });
+        },
+        controller: _controller,
+        children: <Widget>[
+          _pageItem("Enero", 0),
+          _pageItem("Febrero", 1),
+          _pageItem("Marzo", 2),
+          _pageItem("Abril", 3),
+          _pageItem("Mayo", 4),
+          _pageItem("Junio", 5),
+          _pageItem("Julio", 6),
+          _pageItem("Agosto", 7),
+          _pageItem("Septiembre", 8),
+          _pageItem("Octubre", 9),
+          _pageItem("Noviembre", 10),
+          _pageItem("Diciembre", 11),
+        ],
+      ),
+    );
+  }
+
+  Widget _expenses() {
+    return Column(
+      children: const <Widget>[
+        Text("\$2361,42",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40.0,
+          ),
+        ),
+        Text("Total expenses",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+            color: Colors.blueGrey,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _graph() => Container();
+
+  Widget _item(IconData icon, String name, int percent, double value) {
+    return ListTile(
+      leading: Icon(icon, size: 32.0,),
+      title: Text(name,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0
+        ),
+      ),
+      subtitle: Text("$percent% of expenses",
+        style: const TextStyle(
+          fontSize: 16.0,
+          color: Color.fromARGB(101, 101, 101, 1),
+        ),
+      ),
+      trailing: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromRGBO(13, 11, 38, 1),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("\$$value",
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _list() {
+    return Expanded(
+      child: ListView.separated(
+        itemCount: 15,
+        itemBuilder: (BuildContext context, int index) =>
+            // ignore: deprecated_member_use
+            _item(FontAwesomeIcons.bookOpen, "Educación", 14, 145.12),
+            
+        separatorBuilder: (BuildContext context, int index) {
+          return Container(
+            color: const Color.fromRGBO(13, 11, 38, 1),
+            height: 8.0,
+          );
+        },
+      ),
+    );
   }
 }
